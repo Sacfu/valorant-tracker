9a4b39cee6ed5dab8b44c66173721503f55e1f0f
Sacfu <sacfu1@yahoo.com>
2022-10-12 02:41:23 -0400

    added teammate filtering from all players, teammate in game info to jsx

--- src/App.js
+++ src/App.js
@@ -14,50 +14,146 @@ function App() {
   console.log(searchText)
   console.log(playerTag);
 
+  //Grabs basic player info from API
   async function searchForPlayer(event){
     var APICallString = "https://api.henrikdev.xyz/valorant/v1/account/" + searchText + "/" + playerTag;
      await axios.get(APICallString).then(function(response) {
    setPlayerData(response.data.data);
    getRankedData();
+   //console.log(JSON.stringify(playerData) + "this is playerdata")
     }).catch(function(error){
       console.log(error);
     })
   }
-  // console.log(playerData)
 
+  //Uses another API call to retrieve ranked history
   async function getRankedData(){
     var APIRankedData = "https://api.henrikdev.xyz/valorant/v1/mmr/na/"  + searchText + "/" + playerTag;
     await axios.get(APIRankedData).then(function(response){
       setRankedData(response.data.data);
       getMatchHistory();
+      console.log(rankedData)
+      console.log('ranked data above')
     }).catch(function(error){
       console.log(error);
     })
   }
-  // console.log(rankedData);
 
+  //API call for match history data
   async function getMatchHistory(){
     var APIMatchHistory = "https://api.henrikdev.xyz/valorant/v3/matches/na/"  + searchText + "/" + playerTag;
     await axios.get(APIMatchHistory).then(function(response){
       setMatchHistory(response.data.data);
+     console.log(matchHistory)
+     console.log('match history above')
     }).catch(function(error){
       console.log(error);
     })
   }
   console.log(matchHistory[0])
-
-  // var playerPuuid = {
-  //   puuid: playerData.puuid,
-  // }
-  // var gameOneStats = matchHistory[0].players.all_players;
   
-  // const filterResults = gameOneStats.filter(element => {
+//Sorts info from players in a game to match the player that was
+ var recentlyPlayedCharacter;
+ var killsAmount;
+ var deathsAmount;
+ var characterImage;
+ var redTeam = false;
+ 
 
-  //   //return element.id === playerPuuid;
-  //   return element.puuid === '958f7239-83ba-5cdd-9d80-337b10b6a5ed'
-  // });
+ function lastPlayedGame(){
+  
+  const gameOneStats = matchHistory[0].players.all_players;
+  console.log(gameOneStats)
+  console.log('gameone stats above')
+   const filterResults = gameOneStats.filter(element => {
+    return element.puuid === playerData.puuid;
+  });
+  if(filterResults[0] === undefined){
+    const filterResults = gameOneStats.filter(element => {
+      return element.puuid === playerData.puuid;
+    }); return filterResults;
+  } else {
+    recentlyPlayedCharacter = filterResults[0].character;
+    killsAmount = filterResults[0].stats.kills;
+    deathsAmount = filterResults[0].stats.deaths;
+    characterImage = filterResults[0].assets.agent.full;
+  }
+  
+  console.log(filterResults)
+  console.log('filter results above')
+ 
+  if (filterResults[0].team === 'Red'){
+    redTeam = true;
+  }
+ 
+  console.log(redTeam)
+  console.log(JSON.stringify(filterResults) + 'ffiiiii')
+  console.log(characterImage + 'charr')
+}
 
-  // console.log("hi " + JSON.stringify(filterResults) + "hi");
+var filterTeammates = [];
+var filterEnemies = [];
+
+
+function showFriendlyTeam(){
+  var gameOneStats = matchHistory[0].players.all_players;
+  
+    filterTeammates = gameOneStats.filter(element => {
+    if(redTeam === true){
+     if (element.team === 'Red'){
+      return element.team
+      //can change below if statement to show enemies
+     } else if (element.team === 'Blue'){
+      return;
+     }
+    }
+    else if(redTeam === false){
+      if(element.team === 'Blue'){
+        return element.team === 'Blue';
+      //can change below if statement to show enemies
+    } else if(element.team === 'Red'){
+      return;
+    }
+  }
+});
+  console.log(filterTeammates);
+  console.log("filtered team above")
+//traverse array to get info
+
+//Grab specific info from friendly teammates
+
+
+// console.log(mateNames)
+// console.log("mate names above")
+// console.log(mateTags)
+// console.log("mate tags above")
+
+}
+
+// const mateNames = filterTeammates.map((a) => {
+//   return {name:a.name}
+// });
+// const mateTags = filterTeammates.map((a) => {
+//   return {tag:a.tag}
+// });
+// const mateCharacter = filterTeammates.map((a) => {
+//   return {character:a.character}
+// });
+// const mateKills = filterTeammates.map((a) => {
+//   return {kills:a.kills}
+// });
+// const mateDeaths = filterTeammates.map((a) => {
+//   return {deaths:a.deaths}
+// });
+
+
+
+
+console.log((filterEnemies) + "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+
+
+
 
   return (
     <div className="App">
@@ -65,9 +161,11 @@ function App() {
       <input type = 'text' placeholder="username" onChange = {e => setSearchText(e.target.value)}></input>
       <input type = 'text' placeholder='playertag' onChange = {e => setPlayerTag(e.target.value)}></input>
       <button onClick = {e => {searchForPlayer(e);}}>Search for Player</button>
-      {(JSON.stringify(playerData) && JSON.stringify(rankedData) && JSON.stringify(matchHistory))  != '{}' ? 
+      {(matchHistory) !== '[]' && (JSON.stringify(playerData) && JSON.stringify(rankedData) && JSON.stringify(matchHistory))  !== '{}' ? 
       
       <>
+      {lastPlayedGame()}
+      {showFriendlyTeam()}
       
       <p>{playerData.name}</p>
       <p>Account level: {playerData.account_level}</p>
@@ -76,15 +174,43 @@ function App() {
       <p>Last Played Game:</p> 
       <p>{matchHistory[0].metadata.map}</p>
       <p>{matchHistory[0].metadata.mode}</p>
+      <h5> Recent Game Data: </h5>
+      <p> Agent played: {JSON.stringify(recentlyPlayedCharacter)} </p>
+      <img width="400" height = "400" src={characterImage}></img>
+      <p>Kills: {killsAmount}</p>
+      <p>Deaths: {deathsAmount}</p>
       
-      <p>{matchHistory[0].players.all_players[0].character}</p>
-      
-      
+      <div className="container">  
+        <h1> Team Composition </h1>  
+     
+        <table className="table table-bordered">  
+            <tr>  
+                <th>name</th>  
+                <th>tag</th>  
+                <th>character</th>  
+                <th>kills</th>
+                <th>deaths</th>
+            </tr>  
+    
+            {filterTeammates.map((player, index) => (  
+              <tr data-index={index}>  
+                <td>{player.name}</td>  
+                <td>{player.tag}</td>  
+                <td>{player.character}</td>  
+                <td>{player.stats.kills}</td>
+                <td>{player.stats.deaths}</td>
+              </tr>  
+            ))}  
+    
+        </table>  
+    
+    </div>  
+
       </> : 
       <><p>No data available! Try another player.</p></>
       }
 
-      <h5> Ranked Data </h5>
+      
       
     </div>
     
